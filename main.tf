6a6236ba54f89d288397f6c5da9e3dc6c21882e1
Jakob <jakob.ondrey@yello.co>
2023-01-28 20:30:47 -0600

    test lambda

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ module "lambda_function" {
   function_name = "WaybackArchiver"
   description   = "My awesome lambda function"
   handler       = "index.lambda_handler"
-  runtime       = "python3.9"
+  runtime       = "python3.10"
   source_path   = "./wayback_app"
 
 
